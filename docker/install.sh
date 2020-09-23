50fa8f736a8350d6740230156360ad845dc907ef
Tobi <tobiashund6112@gmail.com>
2020-09-23 17:32:51 +0200

    fix typo of pi username

--- docker/install.sh
+++ docker/install.sh
@@ -7,7 +7,7 @@ apt-get -y upgrade
 apt-get install -y git libffi-dev libssl-dev python3 python3-pip
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
-usermod -aG docker Pi
+usermod -aG docker pi
 pip3 -v install docker-compose
 git clone https://github.com/respeaker/seeed-voicecard.git
 cd seeed-voicecard
