664836d993aea560bcfb3f12235f4b7340fe2de5
Tobi <22715034+tobi6112@users.noreply.github.com>
2020-10-14 12:39:40 +0100

    create light api

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -3,26 +3,7 @@ import os
 import logging
 import time
 
-mqtt_host = os.getenv('MQTT_HOST', 'raspberrypi')
-mqtt_port = os.getenv('MQTT_PORT', 1883)
-mqtt_username = os.getenv('MQTT_USERNAME', None)
-mqtt_password = os.getenv('MQTT_PASSWORD', None)
-
-client = mqtt.Client()
-
-logger = logging.getLogger('mqtt')
-
-
-
-def on_connect(client, userdata, flags, rc):
-    logger.info("Connected to MQTT Broker (result_code=%s)", str(rc))
-
-
-def connect():
-    if mqtt_host and mqtt_port:
-        if mqtt_username is not None:
-            client.username_pw_set(mqtt_username, mqtt_password)
-        client.connect(mqtt_host, mqtt_port)
-    client.on_connect = on_connect
-
-    return client
+MQTT_HOST = os.getenv('MQTT_HOST', 'raspberrypi')
+MQTT_PORT = os.getenv('MQTT_PORT', 1883)
+MQTT_USERNAME = os.getenv('MQTT_USERNAME', None)
+MQTT_PASSWORD = os.getenv('MQTT_PASSWORD', None) 
--- src/api/light.py
+++ src/api/light.py
@@ -1,13 +1,25 @@
 from api.app import app
-from api import client
-from flask import request, Response
+from api import MQTT_HOST, MQTT_PORT
+from flask import request, Response, jsonify
+import json
+import paho.mqtt.publish as publish
 
 
 @app.route('/light/<string:friendly_name>/set', methods=['POST'])
-def set_light(friendly_name):
+def set_light_with_friendly_name(friendly_name):
     body = request.get_json()
     if body is None:
-        return "Bad Request"
+        return jsonify({"error": "BAD_REQUEST"}), 400
     else:
-        return body
+        publish.single('zigbee2mqtt/' + friendly_name + '/set', json.dumps(body), hostname=MQTT_HOST, port=MQTT_PORT)
+        return jsonify(body), 200
+
+@app.route('/light/set', methods=['POST'])
+def set_light():
+    body = request.get_json()
+    if body is None or 'friendly_name' not in body or 'payload' not in body:
+        return jsonify({"error": "BAD_REQUEST"}), 400
+    else:
+        publish.single('zigbee2mqtt/' + body['friendly_name'] + '/set', json.dumps(body['payload']), hostname=MQTT_HOST, port=MQTT_PORT)
+        return jsonify(body), 200
 
