7897ef20e128dd420d087840885f84818ac8609d
Tobi <22715034+tobi6112@users.noreply.github.com>
2020-10-15 06:44:55 +0100

    fix tests due to refactoring api

--- src/api/tests/test_light.py
+++ src/api/tests/test_light.py
@@ -1,24 +1,6 @@
 import json
-from api.light import Zigbee2MqttSet, Zigbee2MqttSetEncoder, Payload, zigbee2MqttSetDecoder, PayloadEncoder
 from api.app import app
 import paho.mqtt.publish
-
-# Test wheter the mapping classes can be successfully instantiated 
-def test_new_Zigbee2MqttSet():
-    zigbee2MqttSet = Zigbee2MqttSet(friendly_name='living_room', payload=Payload(state='ON', brightness=255, color='#ffffff'))
-    assert zigbee2MqttSet.friendly_name == 'living_room'
-    assert zigbee2MqttSet.topic == 'zigbee2mqtt/living_room/set'
-    assert zigbee2MqttSet.payload.state == 'ON'
-    assert zigbee2MqttSet.payload.brightness == 255
-    assert zigbee2MqttSet.payload.color == '#ffffff'
-
-# Test the JSON encoding and decoding
-def test_json_encoding_decoding_Zigbee2MqttSet():
-    zigbee2MqttSet = Zigbee2MqttSet(friendly_name='living_room', payload=Payload(state='ON', brightness=255, color='#ffffff'))
-    zigbee2MqttSet_string = json.dumps(zigbee2MqttSet, cls=Zigbee2MqttSetEncoder)
-    zigbee2MqttSet_dict = json.loads(zigbee2MqttSet_string)
-    zigbee2MqttSet_decoded = zigbee2MqttSetDecoder(zigbee2MqttSet_dict)
-    assert zigbee2MqttSet == zigbee2MqttSet_decoded 
 
 # A valid request should return HTTP status OK
 def test_light_endpoint_status_ok(mocker):
@@ -39,6 +21,15 @@ def test_light_endpoint_status_bad_request():
     response = app.test_client().post('/light/set')
     assert response.status_code == 400
 
+# Sort a dictionary
+def ordered(obj):
+    if isinstance(obj, dict):
+        return sorted((k, ordered(v)) for k, v in obj.items())
+    if isinstance(obj, list):
+        return sorted(ordered(x) for x in obj)
+    else:
+        return obj
+
 # A valid request should return ???? TODO 
 def test_light_endpoint_response_data(mocker):
     mocker.patch('paho.mqtt.publish.single') # Prevent from publishing a real message
@@ -52,15 +43,14 @@ def test_light_endpoint_response_data(mocker):
             'color': '#0000ff'
         }
     })
-    assert json.loads(response.data) == json.loads("""{
+    assert ordered(json.loads(response.data)) == ordered(json.loads("""{
         "friendly_name": "living_room",
-        "topic": "zigbee2mqtt/living_room/set",
         "payload": {
             "state": "ON",
             "brightness": 255,
             "color": "#0000ff"
         }
-    }""")
+    }"""))
 
 # A valid request should publish a MQTT message
 def test_light_endpoint_mqtt_params(mocker):
@@ -76,9 +66,9 @@ def test_light_endpoint_mqtt_params(mocker):
     })
 
     expected_payload = {
-            'state': 'ON',
             'brightness': 255,
-            'color': '#0000ff'
+            'color': '#0000ff',
+            'state': 'ON'
         }
 
     paho.mqtt.publish.single.assert_called_once_with('zigbee2mqtt/living_room/set', json.dumps(expected_payload), hostname=mocker.ANY, port=mocker.ANY) # pylint: disable=no-member
