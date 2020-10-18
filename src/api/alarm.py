b3c76b50da4431207d843bd6339f04a2966400b3
Stefan Gimborn <stefan.gimborn@pssg-solutions.de>
2020-10-18 21:08:59 +0200

    improved alarm

--- src/api/alarm.py
+++ src/api/alarm.py
@@ -3,7 +3,9 @@ from app import app, get_db, query_db
 import time
 import paho.mqtt.publish as publish
 import paho.mqtt.subscribe as subscribe
+import paho.mqtt.unsubscribe as unsubscribe
 import datetime
+import random
 
 @app.route('/alarm', methods=['POST', 'DELETE', 'GET'])
 def alarm():
@@ -14,6 +16,7 @@ def alarm():
     
     elif request.method == 'DELETE':
         get_db().execute('DELETE FROM alarms WHERE id=?', [request.form['id']])
+        get_db().commit()
 
     results = []
     alarms = query_db('SELECT * FROM alarms')
@@ -25,19 +28,26 @@ def alarm_play(alarm):
     data = in_file.read()
     in_file.close()
     
-    publish.single("hermes/audioServer/alert/playBytes/fff", data, hostname="mosquitto")
+    publish.single("hermes/audioServer/default/playBytes/alarm-" + alarm['id'], data, hostname="mosquitto")
 
-def on_play_finished():
-    now = datetime.datetime.now()
-    return str(now.hour)
-    alarm = query_db('SELECT * FROM alarms WHERE id=0', (), True)
-    return str(alarm['hours'])
+def on_play_finished(client, userdata, message):
+    active = random.randint(0,1)
+    id = message.payload.id
+    if 'alarm-' in id:
+        alarmId = id.split("-")[1]
+        alarm = query_db("SELECT * FROM alarms WHERE id=?", alarmId, True)
+        if active == 1:
+            alarm_play(alarm)
+        else:
+            unsubscribe("hermes/audioServer/default/playFinished")
+            get_db().execute("UPDATE alarms SET active=1 WHERE id=?", alarmId)
+
 
 @app.cli.command()
 def check_alarm():
     now = datetime.datetime.now()
 
-    alarm = query_db('SELECT * FROM alarms WHERE hours=? AND minutes=?', (now.hour, now.minute), True)
+    alarm = query_db('SELECT * FROM alarms WHERE hours=? AND minutes=?', [now.hour, now.minute], True)
 
     if alarm is None:
         return
